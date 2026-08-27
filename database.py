1bbf6ad9e42777ba86d36b6fe4083aa9e2b2b5e9
knight <snehawq2007@gmail.com>
2026-08-28 02:05:20 +0530

    Organize code into Backend folder

---
+++ database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+DB_config = "gembid.db"
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_config, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS bid_evalution (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        filename VARCHAR(255) NOT NULL,
+        tender_id VARCHAR(255) DEFAULT 'Not specified',
+        years_of_experience INT DEFAULT 0,
+        turnover_amount FLOAT DEFAULT 0,
+        verdict VARCHAR(50) NOT NULL,
+        passed_checks TEXT DEFAULT '[]',
+        failed_checks TEXT DEFAULT '[]',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+    conn.commit()
+    conn.close()
+    
+if __name__ == "__main__":
+    init_db()
