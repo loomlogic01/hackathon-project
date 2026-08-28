8337368d8df3d441b4bf63edac4fb85781c3f33f
knight <snehawq2007@gmail.com>
2026-08-28 19:26:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -11,13 +11,12 @@ def init_db():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS bid_evalution (
+    CREATE TABLE IF NOT EXISTS bid_evaluation (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         filename VARCHAR(255) NOT NULL,
         tender_id VARCHAR(255) DEFAULT 'Not specified',
         years_of_experience INT DEFAULT 0,
         turnover_amount FLOAT DEFAULT 0,
-        verdict VARCHAR(50) NOT NULL,
         passed_checks TEXT DEFAULT '[]',
         failed_checks TEXT DEFAULT '[]',
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
